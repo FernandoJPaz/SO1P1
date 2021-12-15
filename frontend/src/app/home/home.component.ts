0b51c76da42d01aface9da3c4255345f99390bae
FernandoPaz <fernandopaz1995@gmail.com>
2021-12-15 17:57:47 -0600

    Monitor Memoria Ram

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from  '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  private url = 'http://127.0.0.1:3000/ram';
+  object: any;
+  ram: any[] = [];
+  ram_free: any[] = [];
+  ram_percent: any[] = [];
+  ram_usage: any[] = [];
+
+  constructor(public http: HttpClient) { }
+
+  public getData() {
+
+    this.http.get<any[]>(this.url)
+    .subscribe(data => {
+
+      this.object = data
+      this.ram = this.object['ram']
+      this.ram_free = this.object['ram_free']
+      this.ram_percent = this.object['ram_percent']
+      this.ram_usage = this.object['ram_usage']
+
+      console.log(this.object)
+
+    }, error => {
+      console.log(error)
+    });
+  }
 
   ngOnInit(): void {
+    this.getData()
   }
 
 }
